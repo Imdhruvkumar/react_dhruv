1e71b9b48c55c49167b9193fa03d295474b58d9a
imdhruvkumar <dk0495682@gmail.com>
2025-08-02 16:32:35 +0530

    UI IN Progress

--- reactrouter/src/comoponents/Header.jsx
+++ reactrouter/src/comoponents/Header.jsx
@@ -1,7 +1,19 @@
+import {Link, NavLink} from 'react-router-dom'
 export default function Header(){
     return(
         <>
-        <h1 className=" h-screen flex  mt-4 justify-center text-orange-700 text-3xl ">dhruv kumar </h1>
+        <header className="shadow sticky z-50 top-0">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl"></div>
+                    <Link className="flex items-center">
+                    <img 
+                    src="https://cdn.pixabay.com/photo/2023/01/31/01/50/crown-7756927_1280.png"
+                    alt="logo" 
+                    />
+                    </Link>
+            </nav>
+
+        </header>
         </>
     )
 }
