77a208a7fa0808862a05e3db1026103e32393e51
imdhruvkumar <dk0495682@gmail.com>
2025-08-02 13:03:03 +0530

    reactrouter

---
+++ reactrouter/src/comoponents/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <>
+        <h1 className=" h-screen flex  mt-4 justify-center text-orange-700 text-3xl ">dhruv kumar </h1>
+        </>
+    )
+}
