bdf7f96165113f836afe093f72989c2634e44a30
imdhruvkumar <dk0495682@gmail.com>
2025-08-11 19:37:19 +0530

    Final commit of this ContextAPI projrct

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,24 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './Components/Themes'
+import ThemeBtn from './Components/ThemeBtn'
+import Card from './Components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+  const darkTheme= ()=> {
+    setThemeMode("dark")
+  }
+  const lightTheme= () => {
+    setThemeMode("light")
+  }
   
+  useEffect(() => { 
+    document.querySelector('html').classList.remove("light", "dark")
+     document.querySelector('html').classList.add(themeMode)
+
+  },[themeMode])
+
 
   return (
-    
-<div className="flex flex-wrap min-h-screen items-center">
+  <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+     <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
+                        <ThemeBtn/>
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       
+                       <Card/>
                     </div>
                 </div>
-            </div>
+     </div>
+   </ThemeProvider> 
   )
 }
 
