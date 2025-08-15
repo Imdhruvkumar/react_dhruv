168498861ef6212b3430f50b19ea3c85487bc436
imdhruvkumar <dk0495682@gmail.com>
2025-08-15 12:01:46 +0530

    Last Updation of this project

--- reduxtoolkittodo/src/App.jsx
+++ reduxtoolkittodo/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Todos from './components/Todos'
 import './App.css'
+import AddTodo from './components/AddTodo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>learn about redux toolkit</h1>
+    < >
+    <div className='bg-grey-600'>  <h1>learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+      </div>
     </>
   )
 }
