ccac8d4d41a460aab4a303418f8dd4fcefc097f8
imdhruvkumar <dk0495682@gmail.com>
2025-08-19 13:09:34 +0530

    Configure redux toolkit in project

--- Megablog/src/App.jsx
+++ Megablog/src/App.jsx
@@ -1,14 +1,38 @@
-
+import {useDispatch} from 'react-redux'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import authService from "./appwrite/auth"
+import {Header, Footer} from './components'
+import {login,logout} from './store/authSlice'
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  const  [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+    useEffect(()=>{
+      authService.getCurruntUser()
+      .then((userData)=>{
+       if(userData){
+         dispatch(login({userData}))
+       } else{
+         dispatch(logout())
+       }
+      })
+      .finally(setLoading(false))
+    },[])
   
-  return (
-    <>
-      <h1>A blog with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap
+     content-between bg-gray-400">
+      <div className='w-full block'>
+        <Header/>
+
+        <main>/*outlet*/</main>
+
+        <Footer/>
+      </div>
+     </div>
+  ): null
 }
 
 export default App
